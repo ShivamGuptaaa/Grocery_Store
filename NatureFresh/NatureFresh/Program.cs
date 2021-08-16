4acae26c9929c3ee5ee7e0e4700d071db3221ae1
PratikshaPoshe <poshepratiksha612@gmail.com>
2021-08-16 12:58:47 -0700

    replaced if block with switch

--- NatureFresh/NatureFresh/Program.cs
+++ NatureFresh/NatureFresh/Program.cs
@@ -18,18 +18,31 @@ namespace NatureFresh
             customer.Pincode = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter Your Mobile Number");
             customer.MobNum = Console.ReadLine();
-            Console.Write("\nPlease choose a Location to order -\n press <0> for Dadar\n press <1> for Thane\n press <2> for Panvel\n press <3> for Chembur\n press <4> for Goregaon");
+            Console.Write("\nPlease choose a Location to order -\n press <0> for Dadar\n press <1> for Thane\n press <2> for Panvel\n press <3> for Chembur\n press <4> for Goregaon\n");
             int locationOption = int.Parse(Console.ReadLine());
-            if (locationOption == 0)
-                customer.Location = Convert.ToString(Location.Dadar);
-            else if(locationOption == 1)
-                customer.Location = Convert.ToString(Location.Thane);
-            else if (locationOption == 2)
-                customer.Location = Convert.ToString(Location.Panvel);
-            else if (locationOption == 3)
-                customer.Location = Convert.ToString(Location.Chembur);
-            else if (locationOption == 4)
-                customer.Location = Convert.ToString(Location.Dadar);
+
+            switch(locationOption)
+            {
+
+                case 0:
+                    customer.Location = Convert.ToString(Location.Dadar);
+                    break;
+                case 1:
+                    customer.Location = Convert.ToString(Location.Thane);
+                    break;
+                case 2:
+                    customer.Location = Convert.ToString(Location.Panvel);
+                    break;
+                case 3:
+                    customer.Location = Convert.ToString(Location.Chembur);
+                    break;
+                case 4:
+                    customer.Location = Convert.ToString(Location.Goregaon);
+                    break;
+                default:
+                    customer.Location = Convert.ToString(Location.Dadar);
+                    break;
+            }
 
             Console.WriteLine($"{customer.Id} {customer.Name} {customer.Address} {customer.Pincode} {customer.MobNum} {customer.Location}");
         }
