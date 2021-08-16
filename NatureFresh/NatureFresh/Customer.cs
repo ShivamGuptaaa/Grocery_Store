c6630daab356f6b219142e26a00c43be81ba3981
PratikshaPoshe <poshepratiksha612@gmail.com>
2021-08-16 10:40:39 -0700

    Pushed modified Program.cs and customer.cs

---
+++ NatureFresh/NatureFresh/Customer.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NatureFresh
+{
+    class Customer
+    {
+        int id;
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+       
+       
+        string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        string address;
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                address = value;
+            }
+        }
+
+        int pincode;
+        public int Pincode
+        {
+            get
+            {
+                return pincode;
+            }
+            set
+            {
+                pincode = value;
+            }
+        }
+
+        string mobNum;
+        public string MobNum
+        {
+            get
+            {
+                return mobNum;
+            }
+            set
+            {
+                mobNum = value;
+            }
+        }
+
+        public List<string> chooseLocation()
+        {
+            List<string> loc = new List<string>();
+            loc.Add(Convert.ToString(Location.Dadar));
+            loc.Add(Convert.ToString(Location.Thane));
+            loc.Add(Convert.ToString(Location.Panvel));
+            loc.Add(Convert.ToString(Location.chembur));
+            return loc;
+        }
+
+        /*List<string> location = new List<string>();
+        location.Add*/
+
+        public Location location { 
+            get
+            {
+                return location;
+            }
+            set
+            {
+                location = value;
+            } 
+          }
+
+        public enum Location
+        {
+            Dadar, Thane, Panvel, chembur, Goregaon
+        }
+    }
+}
--- NatureFresh/NatureFresh/Program.cs
+++ NatureFresh/NatureFresh/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NatureFresh
 {
@@ -6,7 +7,31 @@ namespace NatureFresh
     {
         static void Main(string[] args)
         {
-          // To do code here
+            Customer customer = new Customer();
+            Console.WriteLine("Enter ID");
+            customer.Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Your name");
+            customer.Name=Console.ReadLine();
+            Console.WriteLine("Enter Your address");
+            customer.Address = Console.ReadLine();
+            Console.WriteLine("Enter Your pincode");
+            customer.Pincode = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Your Mobile Number");
+            customer.MobNum = Console.ReadLine();
+            Console.Write("\nPlease choose a Location to order -\n press <0> for Dadar\n press <1> for Thane\n press <2> for Panvel\n press <3> for Chembur\n press <4> for Goregaon");
+            int locationOption = int.Parse(Console.ReadLine());
+            if (locationOption == 0)
+                customer.location = Customer.Location.Dadar;
+            else if(locationOption == 1)
+                customer.location = Customer.Location.Thane;
+            else if (locationOption == 2)
+                customer.location = Customer.Location.Panvel;
+            else if (locationOption == 3)
+                customer.location = Customer.Location.chembur;
+            else if (locationOption == 4)
+                customer.location = Customer.Location.Goregaon;
+
+
         }
     }
 }
