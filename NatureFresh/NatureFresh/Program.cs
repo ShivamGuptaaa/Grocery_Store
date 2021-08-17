974532bc9f17aec2a0dfbb94b3966f6a3e6fc3d7
unknown <sanahaju123@gmail.com>
2021-08-17 12:28:46 +0530

    updated

--- NatureFresh/NatureFresh/Program.cs
+++ NatureFresh/NatureFresh/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+
 
 namespace NatureFresh
 {
@@ -6,7 +8,22 @@ namespace NatureFresh
     {
         static void Main(string[] args)
         {
-          // To do code here
+
+
+            Validation val = new Validation();
+            Console.Write("Please enter your name:");
+            val.name = Console.ReadLine();
+            Console.Write("Please enter item weight with quantity: ");
+            val.quantity = Console.ReadLine();
+            val.weightOf = Console.ReadLine();
+            Console.Write("\nPlease select store Location: \n1.Panvel\n2.Chembur\n3.Thane\n4.Goregaon\n5.Dadar\n");
+            val.loc = Console.ReadLine();
+            Console.Write("\nPlease enter your 10 digit mobile number:");
+            val.phoneNumber = Console.ReadLine();
+            Console.Write("\nPlease enter your pincode:");
+            val.pinCode = Console.ReadLine();
+
+            Console.Write(val.GetDetails());
         }
     }
 }
